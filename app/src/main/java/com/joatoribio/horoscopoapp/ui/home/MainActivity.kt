8eab451742fd1a77e8ee7e955268d8b5ebb7bdf1
Joandry <joatoribiocompras@gmail.com>
2025-03-12 13:45:25 -0400

    initial commit

---
+++ app/src/main/java/com/joatoribio/horoscopoapp/ui/home/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.joatoribio.horoscopoapp.ui.home
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.joatoribio.horoscopoapp.R
+import com.joatoribio.horoscopoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        iniUI()
+    }
+
+    private fun iniUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/joatoribio/horoscopoapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,26 @@
+package com.joatoribio.horoscopoapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.joatoribio.horoscopoapp.R
+import com.joatoribio.horoscopoapp.databinding.FragmentHoroscopoBinding
+import com.joatoribio.horoscopoapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
